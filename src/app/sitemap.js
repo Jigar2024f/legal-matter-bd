1920660a8b4fa151e589a8ece6759539d42757f1
Abdullah Al Mansur <almahfuz2019@gmail.com>
2024-11-08 00:29:37 +0600

    added sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,5 @@
-import { projects } from "../../public/Data/projects";
-
 export default function sitemap() {
-  const baseUrl = "https://www.almahfuz.me";
+  const baseUrl = "https://www.legalmatterbd.com/";
 
   // Static pages for sitemap
   const pages = [
@@ -12,70 +10,13 @@ export default function sitemap() {
       priority: 1,
     },
     {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/resume`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/works`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
       url: `${baseUrl}/blogs`,
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
       priority: 0.8,
     },
   ];
 
-  // Images for sitemap
-  const images = Array.from({ length: 34 }, (_, index) => ({
-    loc: `${baseUrl}/images/google/abdullah_al_mahfuz_image${index + 1}.jpg`,
-    title: `Abdullah Al Mahfuz - Frontend Developer Image ${index + 1}`,
-    caption: `Showcasing my journey as a dedicated frontend developer, this is Image ${
-      index + 1
-    }, highlighting my expertise in creating visually appealing and responsive web applications.`,
-  }));
-
-  // Projects for sitemap
-  const myProjects = projects.map((project) => ({
-    url: `${baseUrl}/works/${project.slug}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.7,
-  }));
-
   // Combine all the data
-  return [
-    ...pages,
-    ...myProjects,
-    ...images.map((image) => ({
-      url: image.loc,
-      images: [
-        {
-          url: image.loc,
-          title: image.title,
-          caption: image.caption,
-        },
-      ],
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.3,
-    })),
-  ];
+  return [...pages];
 }
