80d7ab2d4e0cba341fadef510fd76f15c26f07d1
Abdullah Al Mansur <almahfuz2019@gmail.com>
2024-11-09 00:06:33 +0600

    update hover2231

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,9 +16,71 @@ const notoSerifBengali = Noto_Serif_Bengali({
 });
 
 export const metadata = {
-  title: "Legal Matter BD",
+  title: "Legal Matter BD | Legal Services & Solutions in Bangladesh",
+
   description:
-    "Legal Matter BD is here to help you with your legal issues. Our experienced team works by your side, making the process simpler, and giving you clear advice and practical solutions so you can feel confident moving forward.",
+    "Legal Matter BD provides reliable and professional legal services, tailored to address diverse legal needs across Bangladesh. Explore our solutions for justice, legal compliance, and expert guidance with a customer-centric approach.",
+
+  keywords:
+    "Legal Matter BD, Legal Services, Law Firm, Legal Compliance, Bangladesh Legal Support, Legal Solutions, Justice Assistance, Legal Matters Bangladesh, Legal Expertise, Sylhet Legal Services, Customer-Centric Legal Solutions, Vastu Compliance, Legal Advocacy, Legal Consultancy, Reliable Law Firm, Legal Documentation, Legal Support BD, Local Legal Experts, Court Representation, Legal Assistance Bangladesh, Tax Compliance, Law BD, Vastu Guidance BD, Contract Drafting, Legal Matter Assistance, Family Law, Business Law, Land Law Bangladesh, Legal Help, Expert Legal Services, International Legal Experience BD",
+
+  author: "Legal Matter BD Team",
+
+  openGraph: {
+    title: "Legal Matter BD | Legal Solutions for Every Need",
+    description:
+      "Discover Legal Matter BD, your trusted partner for navigating legal complexities with professional guidance, compliance solutions, and reliable legal support across Bangladesh.",
+    url: "https://legalmatterbd.com",
+    images: [
+      {
+        url: "/public/openGraph.jpg",
+        alt: "Legal Matter BD - Reliable Legal Solutions",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    site_name: "Legal Matter BD - Trusted Legal Services",
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    title: "Legal Matter BD | Expert Legal Solutions in Bangladesh",
+    description:
+      "Explore Legal Matter BD for professional legal services tailored to individual and business needs. Reliable, efficient, and customer-focused solutions for legal matters.",
+    card: "summary_large_image",
+    images: [
+      {
+        url: "/public/openGraph.jpg",
+        alt: "Legal Matter BD Legal Services",
+      },
+    ],
+  },
+
+  robots: "index, follow",
+  canonical: "https://legalmatterbd.com",
+
+  structuredData: {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Legal Matter BD",
+    url: "https://legalmatterbd.com",
+    sameAs: [
+      "https://www.facebook.com/legalmatterbd",
+      "https://github.com/legalmatterbd",
+      "https://www.linkedin.com/company/legalmatterbd",
+      "https://legalmatterbd.com/",
+    ],
+    logo: "/assets/images/legal_matter_bd_logo.png",
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+8801712-581787",
+      email: "info@legalmatterbd.com",
+      contactType: "Customer Service",
+      areaServed: "BD",
+      availableLanguage: ["English", "Bengali"],
+    },
+  },
 };
 
 export default function RootLayout({ children }) {
