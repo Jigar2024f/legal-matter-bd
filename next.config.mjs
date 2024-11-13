877941d00ed319c1f8914093bc3432e32efea9e6
Abdullah Al Mansur <almahfuz2019@gmail.com>
2024-11-13 17:38:09 +0600

    dfads

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ibb.co","i.ibb.co.com", "cdn.gobankingrates.com"],
+    domains: ["i.ibb.co", "cdn.gobankingrates.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Match all paths
+        has: [
+          {
+            type: "host",
+            value: "legalmatterbd.com", // Match requests to the non-www domain without HTTPS
+          },
+        ],
+        destination: "https://www.legalmatterbd.com/:path*", // Redirect to HTTPS www version
+        permanent: true, // 301 redirect
+      },
+    ];
   },
 };
 
