7d832c418b9e66cf6be8b12ea8cf787285faa169
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-11-21 01:52:33 +0600

    update isAuth

--- src/api/isAuth.jsx
+++ src/api/isAuth.jsx
@@ -20,7 +20,7 @@ const isAuth = (Component) => {
               withCredentials: true,
             }
           );
-          setAuth(response.data.success);
+          setAuth(response?.data?.payload?.success);
         } catch (err) {
           setError("Not authenticated");
           setAuth(false);
